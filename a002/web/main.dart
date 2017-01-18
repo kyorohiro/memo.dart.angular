b61fa3bcdfa371ac23a44b2f0e654d6d22f402bb
kyorohiro <kyorohiro@gmail.com>
2017-01-18 16:40:59 +0900

    ..

--- a002/web/main.dart
+++ a002/web/main.dart
@@ -1,9 +1,26 @@
-// Copyright (c) 2017, kyorohiro. All rights reserved. Use of this source code
-// is governed by a BSD-style license that can be found in the LICENSE file.
-
 import 'package:angular2/platform/browser.dart';
-import 'package:a002/app_component.dart';
+import 'package:angular2/core.dart';
+import 'dart:async';
 
 main() {
   bootstrap(AppComponent);
+}
+
+@Component(
+    selector: 'my-app',
+    template: """
+  <div>{{counter}}</div>
+  """
+)
+class AppComponent implements OnInit {
+  int counter = 0;
+
+  ngOnInit() {
+    new Future(() async {
+      while (true) {
+        await new Future.delayed(new Duration(seconds: 1));
+        counter++;
+      }
+    });
+  }
 }
